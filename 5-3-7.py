93330a690301157b48bedd8d900a6ba7b1766d64
Moti Azran <motiazran9@gmail.com>
2021-08-01 21:22:01 +0300

    Fix code

--- 5-3-7.py
+++ 5-3-7.py
@@ -1,2 +1,14 @@
-def chocolate_maker(small, big, x):
-	return small + (big * 5) >= x
+def choclate_maker(small, big, x):
+	if small + (big * 5) < x:
+		return False
+
+	if x == big * 5:
+		return True
+	elif x > big * 5:
+		x = x - big * 5
+		return x <= small
+	else:
+		big_n = int((big * 5) / x)
+		x = x - big_n * 5
+		return x <= small
+
